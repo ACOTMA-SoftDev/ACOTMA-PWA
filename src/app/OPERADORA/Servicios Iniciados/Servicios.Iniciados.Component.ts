21b3548a6bf08733a46ce74c53b4e167ea5e1d96
Cruz0304 <famc0004@outlook.com>
2023-03-09 14:12:24 -0600

    Creation ServiceStart View

---
+++ src/app/OPERADORA/Servicios Iniciados/Servicios.Iniciados.Component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+  templateUrl:'Servicios.Iniciados.Component.html',
+  styleUrls:['Servicios.Iniciados.Component.scss']
+})
+export class ServiciosIniciadosComponent implements OnInit{
+
+
+  ngOnInit(): void {
+  }
+
+}
