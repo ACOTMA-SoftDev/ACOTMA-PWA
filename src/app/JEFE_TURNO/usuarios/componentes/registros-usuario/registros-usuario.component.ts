5fe6bf567307e3f6f2f9e327c9c3a9de3f68f982
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-03-15 06:58:30 -0600

    Add files via upload

--- src/app/JEFE_TURNO/usuarios/componentes/registros-usuario/registros-usuario.component.ts
+++ src/app/JEFE_TURNO/usuarios/componentes/registros-usuario/registros-usuario.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuariosCrudService } from 'src/app/Servicios/usuarios-crud.service'; //importamos la clase desde el directorio
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registros-usuario',
   templateUrl: './registros-usuario.component.html',
-  styleUrls: ['./registros-usuario.component.css']
+  styleUrls: ['./registros-usuario.component.css'],
+  providers: [UsuariosCrudService],
 })
-export class RegistrosUsuarioComponent {
+export class RegistrosUsuarioComponent implements OnInit{
+  //creamos un contrucutor
+  Usuarios:any=[];//recolectamos todos los datos para luego mostrarlos
+  title = 'Usuarios';
+  constructor(private UsuariosCrudService:UsuariosCrudService, private router:Router){}
+
+    ngOnInit(){
+      this.UsuariosCrudService.getUsuarios().subscribe(res =>{
+        this.Usuarios=res;//de esta forma mostramos los datos
+        console.log('Resp',res);
+      })
   
+    }
 
 }
