cd69fb267dabc3452ba953cdfe8667ac762ced20
Cruz0304 <famc0004@outlook.com>
2023-03-21 03:00:29 -0600

    View Verificacion del Dia

--- src/app/ROUTING/app.VerificadoresRouting.ts
+++ src/app/ROUTING/app.VerificadoresRouting.ts
@@ -3,11 +3,12 @@ import { ConsultarServicioComponent } from "../VERIFICADORES/ConsutlarServicio/C
 import { VerificarUnidadesComponent } from "../VERIFICADORES/VerificarUnidad/Verificar.Unidad.Component";
 import {NgModule } from "@angular/core";
 import { ReportarUnidad } from "../VERIFICADORES/ReportarUnidad/Reportar.Unidad.Component";
-
+import { VerificarDia } from "../VERIFICADORES/VerificacionDia/Verificar.Dia.Component";
 const appVerificadoresRouting: Routes=[
     {path:'Verificadores/ConsultaServicio',component:ConsultarServicioComponent},
     {path:'Verificadores/VerificarUnidad', component:VerificarUnidadesComponent},
-    {path:'Verificadores/ReportarUnidad',component:ReportarUnidad}
+    {path:'Verificadores/ReportarUnidad',component:ReportarUnidad},
+    {path:'Verificadores/VerificacionDia',component:VerificarDia},
 
 
 ];
---
+++ src/app/VERIFICADORES/VerificacionDia/Verificar.Dia.Component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+
+  templateUrl:'Verificar.Dia.Component.html'
+  
+})
+
+export class VerificarDia implements OnInit{
+  ngOnInit(): void {
+
+  }
+
+}
