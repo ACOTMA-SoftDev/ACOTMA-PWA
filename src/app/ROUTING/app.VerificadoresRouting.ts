af5eb96dafa3476550aaa5cec0894a8e46b02391
Cruz0304 <famc0004@outlook.com>
2023-03-21 02:28:27 -0600

    View VerificarUnidad

--- src/app/ROUTING/app.VerificadoresRouting.ts
+++ src/app/ROUTING/app.VerificadoresRouting.ts
@@ -1,9 +1,12 @@
 import { RouterModule, Routes } from "@angular/router";
 import { ConsultarServicioComponent } from "../VERIFICADORES/ConsutlarServicio/Consultar.Servicio.Component";
+import { VerificarUnidadesComponent } from "../VERIFICADORES/VerificarUnidad/Verificar.Unidad.Component";
 import {NgModule } from "@angular/core";
 
 const appVerificadoresRouting: Routes=[
-    {path:'Verificadores/ConsultaServicio',component:ConsultarServicioComponent}
+    {path:'Verificadores/ConsultaServicio',component:ConsultarServicioComponent},
+    {path:'Verificadores/VerificarUnidad', component:VerificarUnidadesComponent}
+
 
 ];
 @NgModule({
---
+++ src/app/VERIFICADORES/VerificarUnidad/Verificar.Unidad.Component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+
+  templateUrl:'Verificar.Unidad.Component.html',
+  styleUrls:['Verificar.Unidad.Component.scss']
+
+})
+
+export class VerificarUnidadesComponent implements OnInit{
+  unidad="102"
+
+  ngOnInit(): void {
+
+  }
+
+}
+
+
