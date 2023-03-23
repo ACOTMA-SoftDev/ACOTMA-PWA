89b43be755c1b01bd0e7d256b7305d13e7ad5854
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-03-23 13:58:33 -0600

    actualizar jefe_enturno
    asignar radios form

--- src/app/JEFE_TURNO/asignacion/componentes/asignacion-radios/asignacion-radios.component.ts
+++ src/app/JEFE_TURNO/asignacion/componentes/asignacion-radios/asignacion-radios.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { ResourceLoader } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterPreloader } from '@angular/router';
+import { Location } from '@angular/common';
+import { async } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-asignacion-radios',
@@ -6,5 +13,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./asignacion-radios.component.css']
 })
 export class AsignacionRadiosComponent {
+
+  usuario!: string;
+  Num_Radio!:Int16Array;
+  Codigo_Radio!:string;
+  Tarjeta_Maestra!:string;
+
+
+  constructor(private http:HttpClient, private location: Location, private sanitizer:DomSanitizer){}
+
+  enviarAsignacionRadios(){
+    const datos = {
+      usuario:this.usuario, 
+      Num_Radio:this.Num_Radio,
+      Codigo_Radio:this.Codigo_Radio,
+      Tarjeta_Maestra:this.Tarjeta_Maestra
+    }
+    let url="api/agregar_Asignacion_de_Radios"
+    this.http.post(url,datos).toPromise().then((data:any )=>{
+      console.log(data)
+      location.reload();
+    })
+    
+  }
 
 }
