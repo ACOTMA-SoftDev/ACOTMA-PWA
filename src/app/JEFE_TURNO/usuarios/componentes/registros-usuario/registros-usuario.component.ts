0f6f260a9f8eb497ac2ff3ee1db9711efccb020b
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-03-23 14:27:48 -0600

    Registro de usuarios
    cambio de tabla en el registro de usuarios

--- src/app/JEFE_TURNO/usuarios/componentes/registros-usuario/registros-usuario.component.ts
+++ src/app/JEFE_TURNO/usuarios/componentes/registros-usuario/registros-usuario.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuariosCrudService } from 'src/app/Servicios/usuarios-crud.service'; //importamos la clase desde el directorio
+import { HttpClient } from '@angular/common/http';
+//import { UsuariosCrudService } from 'src/app/Servicios/usuarios-crud.service'; //importamos la clase desde el directorio
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registros-usuario',
   templateUrl: './registros-usuario.component.html',
   styleUrls: ['./registros-usuario.component.css'],
-  providers: [UsuariosCrudService],
+  //providers: [UsuariosCrudService],
 })
 export class RegistrosUsuarioComponent implements OnInit{
   //creamos un contrucutor
   Usuarios:any=[];//recolectamos todos los datos para luego mostrarlos
-  title = 'Usuarios';
-  constructor(private UsuariosCrudService:UsuariosCrudService, private router:Router){}
+  term:any;
 
-    ngOnInit(){
-      this.UsuariosCrudService.getUsuarios().subscribe(res =>{
-        this.Usuarios=res;//de esta forma mostramos los datos
-        console.log('Resp',res);
-      })
-  
-    }
+	constructor(private http:HttpClient) {}
+   //creamos un contrucutor
+   title = 'Registro de usuarios';
+
+   getAllRegistros(){
+
+    this.http.get('api/ver_Registro_de_usuarios').subscribe(data=>{
+      console.log(data)
+      this.Usuarios=data
+    })
+  }
+
+  ngOnInit(): void {
+    this.getAllRegistros();
+  }
 
 }
