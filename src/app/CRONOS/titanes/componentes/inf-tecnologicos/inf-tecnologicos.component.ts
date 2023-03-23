b0b0a175ef2bd1b3564e5e8e135d065ba8f24f08
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-03-23 13:46:30 -0600

    actulaiizacion INCtec
    Mostrar el contenido de tabla de incidencias tecnologicas

--- src/app/CRONOS/titanes/componentes/inf-tecnologicos/inf-tecnologicos.component.ts
+++ src/app/CRONOS/titanes/componentes/inf-tecnologicos/inf-tecnologicos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-inf-tecnologicos',
   templateUrl: './inf-tecnologicos.component.html',
   styleUrls: ['./inf-tecnologicos.component.css']
 })
-export class InfTecnologicosComponent {
+export class InfTecnologicosComponent implements OnInit{
+
+  term:any;
+  InformeIncidenciasTec:any=[];//recolectamos todos los datos para luego mostrarlos
+
+	constructor(private http:HttpClient) {}
+   //creamos un contrucutor
+   title = 'Informe de accidentes';
+
+   getAllEstacionesToday(){
+    this.http.get('api/Informe_de_incidencias_tecnologicas_de_hoy').subscribe(data=>{
+      console.log(data)
+      this.InformeIncidenciasTec=data
+    })
+    }
+
+  ngOnInit(): void {
+    this.getAllEstacionesToday();
+  }
 
 }
