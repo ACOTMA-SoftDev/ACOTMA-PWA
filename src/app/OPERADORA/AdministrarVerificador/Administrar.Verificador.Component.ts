03eeb870e6c10ebbd68aa5e6482ed2558bd726ed
Cruz0304 <famc0004@outlook.com>
2023-03-23 10:52:05 -0600

    View Administrar Verificadores

---
+++ src/app/OPERADORA/AdministrarVerificador/Administrar.Verificador.Component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component
+({
+  templateUrl:'Administrar.Verificador.Component.html',
+  styleUrls:['Administrar.Verificador.Component.scss']
+
+})
+
+export class AdministrarVerificadores implements OnInit{
+  icongif='assets/iconBlackAcotma.gif'
+  ngOnInit(): void {
+  }
+
+}
+
+
