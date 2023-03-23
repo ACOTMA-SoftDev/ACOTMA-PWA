4e30a348e5b5d25a1e478a0327a03687a86d6a24
Cruz0304 <famc0004@outlook.com>
2023-03-23 10:52:33 -0600

    View Agregar Verificadores

---
+++ src/app/OPERADORA/AgregarVerificador/Agregar.Verificador.Component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  templateUrl:'Agregar.Verificador.Component.html',
+  styleUrls:['Agregar.Verificador.Component.scss']
+})
+
+export class AgregarVerificadores implements OnInit
+{
+  icongif='assets/iconBlackAcotma.gif'
+  iconV='assets/iconVerificadores.gif'
+  ngOnInit(): void {
+
+  }
+
+}
