4b0d8f195a85df9aca2dbb8f160da397891262fc
Cruz0304 <famc0004@outlook.com>
2023-03-23 10:53:26 -0600

    Operadora with Reportar Unidad html

--- src/app/ROUTING/app.OperadoraRouting.ts
+++ src/app/ROUTING/app.OperadoraRouting.ts
@@ -6,12 +6,18 @@ import { HorarioComponent } from "../OPERADORA/HorarioServicio/Horario.Servicio.
 import{FormsModule} from '@angular/forms'
 import { EditarServicioComponent } from "../OPERADORA/EditarServicio/Editar.Servicio.Component";
 import {EditarHoraComponent} from "../OPERADORA/EditarHorarioServicio/Editar.Hora.Component";
+import { EditarVerificadorComponent } from "../OPERADORA/EditarVerificadores/Editar.Verificador.Component";
+import { AgregarVerificadores } from "../OPERADORA/AgregarVerificador/Agregar.Verificador.Component";
+import { AdministrarVerificadores } from "../OPERADORA/AdministrarVerificador/Administrar.Verificador.Component";
 const appOperadoraRouting:Routes=[
   {path:'Operadora/Home', component:HomeComponent},
   {path: 'Operadora/ServiciosIniciados',component:ServiciosIniciadosComponent},
   {path: 'Operadora/HorarioServicio',component:HorarioComponent},
   {path: 'Operadora/EditarServicio',component:EditarServicioComponent},
-  {path: 'Operadora/EditarHoraServicio',component:EditarHoraComponent}
+  {path: 'Operadora/EditarHoraServicio',component:EditarHoraComponent},
+  {path:'Operadora/AdminVerificadores',component:AdministrarVerificadores},
+  {path:'Operadora/Agregar/Verificador',component:AgregarVerificadores},
+  {path:'Operadora/Editar/Verificador',component:EditarVerificadorComponent}
 
 ];
 @NgModule({
