a6d80d812faa207891d4f2bb9a9b997121e1c92c
Cruz0304 <famc0004@outlook.com>
2023-03-23 10:52:55 -0600

    View Editar Verificadores

---
+++ src/app/OPERADORA/EditarVerificadores/Editar.Verificador.Component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+ templateUrl:'Editar.Verificador.Component.html',
+ styleUrls:['Editar.Verificador.Component.scss']
+})
+export class EditarVerificadorComponent implements OnInit{
+
+  ngOnInit(): void {
+
+  }
+
+}
