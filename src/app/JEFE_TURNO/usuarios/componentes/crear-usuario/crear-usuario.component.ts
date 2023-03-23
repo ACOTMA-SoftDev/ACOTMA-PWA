8e2c5554cf8c14c4c4cdcba98bbdb468d1324b6b
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-03-23 14:23:45 -0600

    crear usuarios
    actulizar formulario de creacion de usuarios

--- src/app/JEFE_TURNO/usuarios/componentes/crear-usuario/crear-usuario.component.ts
+++ src/app/JEFE_TURNO/usuarios/componentes/crear-usuario/crear-usuario.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ResourceLoader } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterPreloader } from '@angular/router';
+import { Location } from '@angular/common';
+import { async } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-crear-usuario',
   templateUrl: './crear-usuario.component.html',
   styleUrls: ['./crear-usuario.component.css']
 })
-export class CrearUsuarioComponent {
+export class CrearUsuarioComponent{
 
-}
+  usuario!: string;
+  nombre!:string;
+  apellidoP!:string;
+  apellidoM!:string;
+  pass!:string;
+
+  constructor(private http:HttpClient, private location: Location, private sanitizer:DomSanitizer){}
+
+  enviarUsuarios(){
+    const datos = {
+      usuario:this.usuario, 
+      nombre:this.nombre,
+      apellidoP:this.apellidoP,
+      apellidoM:this.apellidoM,
+      pass:this.pass,
+    }
+    let url="api/agregar_usuarios"
+    this.http.post(url,datos).toPromise().then((data:any )=>{
+      console.log(data)
+      location.reload();
+    })
+
+  }
+
+} 
