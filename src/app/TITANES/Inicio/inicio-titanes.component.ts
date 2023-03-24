ee47b0ab2017c7ddf98fb6b65143448053b8e5e8
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-03-23 21:48:38 -0600

    Mostrar mensajes
    codigo ts para llamara a los mensajes

--- src/app/TITANES/Inicio/inicio-titanes.component.ts
+++ src/app/TITANES/Inicio/inicio-titanes.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-inicio-titanes',
   templateUrl: './inicio-titanes.component.html',
   styleUrls: ['./inicio-titanes.component.css']
 })
-export class InicioTitanesComponent {
+export class InicioTitanesComponent implements OnInit {
+
+  Mensajes:any=[];
+
+  constructor ( private http:HttpClient){
+  };
+
+  getMensaje(){
+   this.http.get('api/Publicaciones').subscribe(data=>{
+      console.log(data)
+      this.Mensajes=data
+    })
+  }
+
+  ngOnInit(): void {
+    this.getMensaje();
+  }
 
 }
