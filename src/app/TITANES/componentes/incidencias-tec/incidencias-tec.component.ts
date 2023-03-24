a3f4c1b472e26b3f16ac89fa4aa8c970142602a8
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-03-24 02:18:31 -0600

    incidenasTEC
    adreGue el formulario INCTEC

--- src/app/TITANES/componentes/incidencias-tec/incidencias-tec.component.ts
+++ src/app/TITANES/componentes/incidencias-tec/incidencias-tec.component.ts
@@ -1,4 +1,11 @@
- import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ResourceLoader } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterPreloader } from '@angular/router';
+import { Location } from '@angular/common';
+import { async } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-incidencias-tec',
@@ -6,5 +13,34 @@
   styleUrls: ['./incidencias-tec.component.css']
 })
 export class IncidenciasTecComponent {
+
+  ServicioEst!:string;
+  NEco!:string;
+  EquipoAfectado!:string;
+  IdEquipoA!:string;
+  Falla!:string;
+  Estado=null;
+
+  constructor(private http:HttpClient, private location: Location, private sanitizer:DomSanitizer){}
+
+  enviarInformeTec(){
+    const datos = {
+
+      Servicio:this.ServicioEst,
+      VehiculoECO:this.NEco, 
+      Equipo_afectado:this.EquipoAfectado,
+      Id_equipo_afectado:this.IdEquipoA,
+      Falla:this.Falla,
+      Estado:this. Estado,
+    }
+    let url="api/agregar_nueva_incidencia_tecnologica"
+    this.http.post(url,datos).toPromise().then((data:any)=>{
+      console.log(data)
+      location.reload();
+    })
+    
+  }
+
+
 
 }
