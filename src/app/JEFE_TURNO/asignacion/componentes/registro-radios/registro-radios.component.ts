80f80f04a6d7166237274ff50057986148e4cfe9
William HM <68921925+godwlly5000@users.noreply.github.com>
2023-04-02 08:35:49 -0600

    actualizacion
    actualize el diseño de unos componenetes

--- src/app/JEFE_TURNO/asignacion/componentes/registro-radios/registro-radios.component.ts
+++ src/app/JEFE_TURNO/asignacion/componentes/registro-radios/registro-radios.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-//import { UsuariosCrudService } from 'src/app/Servicios/usuarios-crud.service'; //importamos la clase desde el directorio
-import { Router } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-registro-radios',
@@ -11,10 +11,11 @@ import { Router } from '@angular/router';
 export class RegistroRadiosComponent implements OnInit{
 
   //creamos un contrucutor
-  asignaciones:any=[];//recolectamos todos los datos para luego mostrarlos
+  asignaciones:any;//recolectamos todos los datos para luego mostrarlos
   term:any;
+  Id_asignacionRadio:any;
 
-	constructor(private http:HttpClient) {}
+	constructor(private router: Router, private http: HttpClient,private location:Location) {}
    //creamos un contrucutor
    title = 'Registro de usuarios';
 
@@ -30,6 +31,31 @@ export class RegistroRadiosComponent implements OnInit{
      setInterval(() => {
      this.GetAsignacionRadios();
    }, 500)
-   }
+   };
+
+
+   cerrarModal(){
+    location.reload()
+  }
+
+   btnCancelarAsignacion(Id_asignacionRadiobtn:any){
+    this.Id_asignacionRadio=Id_asignacionRadiobtn
+    console.log(this.Id_asignacionRadio)
+  }
+   btnAccept(){
+    const datos={
+      Id_asignacionRadio:this.Id_asignacionRadio
+    }
+    let url="https://prueba252.somee.com/api/eliminar_Asignacion_de_Radios"
+    this.http.post(url,datos).toPromise().then(data=>{
+      if(data){
+        console.log(data)
+        this.cerrarModal()
+      }
+    })
+
+  }
+
+
   
 }
