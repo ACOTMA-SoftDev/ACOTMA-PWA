ed2568a45af409fcb59ae8869e04c3fdce803d2b
Cruz0304 <famc0004@outlook.com>
2023-04-04 07:02:19 -0600

    Download pdf Cronos despacho

--- src/app/CRONOS/VerificacionDashBoard/Verificacion.Dashboard.Component.ts
+++ src/app/CRONOS/VerificacionDashBoard/Verificacion.Dashboard.Component.ts
@@ -1,5 +1,8 @@
-import { HttpClient, JsonpInterceptor } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
+import { formatDate } from "@angular/common";
+import { HttpClient, } from "@angular/common/http";
+import { Component, OnInit, ViewChild,ElementRef } from "@angular/core";
+import * as jsPDF from 'jspdf'
+import autoTable from "jspdf-autoTable";
 
 @Component
   ({
@@ -51,6 +54,57 @@ export class VerificacionDashboardComponent implements OnInit {
     })
 
   }
+  @ViewChild ('content') content!:ElementRef
+  gettable(){
+    const rows=[]
+    const table=this.content.nativeElement
+    const trs=table.querySelectorAll('tr')
+    for (let index = 0; index < trs.length; index++) {
+      const  row = []
+      const tds=trs[index].querySelectorAll("td,th")
+      for (let j = 0; j < tds.length; j++) {
+        row.push(tds[j].textContent.trim())
+      }
+      rows.push(row)
+    }
+    return rows
+  }
+  descargarPDF(){
+    const doc=new jsPDF.default()
+    const tabledata=this.gettable()
+    autoTable( doc,{
+      head:[tabledata[0]],
+      body:tabledata.slice(1),
+      styles:{
+        cellPadding: 1,
+        fontSize: 8,
+        valign: 'middle',
+        halign: 'center',
+        fillColor: [120, 66, 18],
+        textColor: [0, 0, 0]          //color de texto de la informacion
+      },
+      headStyles: {
+        fillColor: [156, 34, 60],       //color de fondo del encabezado de la tabla
+        textColor: [255, 255, 255],       //color del texto del encabezado
+        fontSize:10,
+        halign: 'center'
+      },
+      margin:{ top:10},
+      didDrawPage: (data) =>{
+        doc.text('', data.settings.margin.left +50, 10);
+      }
+
+    })
+    const fechas = new Date()
+    const dia = fechas.getDay()
+    const mes=fechas.getMonth()
+    const año=fechas.getFullYear()
+    const fecha = `${dia}:${mes}:${año}`
+    doc.save("Unidades en servicio "+fecha)
+
+
+  }
+
   ngOnInit(): void {
     setInterval(() => {
       this.getDataUnidades()
@@ -63,6 +117,6 @@ export class VerificacionDashboardComponent implements OnInit {
       this.GetCiclosPerdidos()
     }, 1000)
   }
-  
+
 }
 
